09c9877ad391b621e8220182d80c3c358b737198
Shahzebk26 <shahzebsoftwareforge@gmail.com>
2025-03-10 14:16:52 +0500

    Add files via upload

---
+++ dumysidebarscript.js
@@ -0,0 +1,9 @@
+// Open Sidebar
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+}
+
+// Close Sidebar
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+}
